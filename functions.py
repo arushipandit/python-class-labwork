e260cf6a6723007725f8992ac7bceb7f7eb98986
arushipandit <56163533+arushipandit@users.noreply.github.com>
2020-08-02 16:50:18 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+#factorial using function
+def factorial(i):
+    fact=0
+    for i in range(1,i+1)
+     fact*=i
+    
+
+
+# In[ ]:
+
+
+#maximun marks out of five student
+def maxmarks(i):
+    input=()
+    marks = [0]
+    inpt = input("Enter the marks for 5 students: ").strip().split()
+    for i in inpt:
+        i=int(i)
+        marks.append(i)
+    print("The maximum marks scored are",max(marks),"by student",marks.index(maxmarks(i)))
+
+    
+
