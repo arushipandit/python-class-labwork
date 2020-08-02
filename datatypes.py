20542e6606bfc985ec32f21c34e2009146cbd017
arushipandit <56163533+arushipandit@users.noreply.github.com>
2020-08-02 16:48:01 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+name= input('what is ur name')
+age=input(Enter age”)
+print(name + “you are” + age +”years old”)
+
+
+# In[6]:
+
+
+n=7   
+type(n)
+
+
+# In[7]:
+
+
+n='hi'
+type(n)
+
+
+# In[11]:
+
+
+print(format('hello', '<30’))
+print(format('hello', '>30'))
+
+
+# In[12]:
+
+
+print(format('hello', '^30'))
+
+
+# In[ ]:
+
+
+
+
