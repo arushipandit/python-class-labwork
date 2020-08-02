da18b9fb8b02e34d3df753dc2c3111d34f67ed5e
arushipandit <56163533+arushipandit@users.noreply.github.com>
2020-08-02 16:47:30 +0530

    Add files via upload

---
+++ intro.py
@@ -0,0 +1,169 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+a=10
+
+
+# In[2]:
+
+
+
+
+
+# In[3]:
+
+
+a+4
+
+
+# In[4]:
+
+
+type(a)
+
+
+# In[5]:
+
+
+a
+
+
+# In[6]:
+
+
+a=8.8
+
+
+# In[7]:
+
+
+type(a)
+
+
+# In[13]:
+
+
+tax=0.1
+pay=10000
+tax=tax*pay
+
+
+# In[14]:
+
+
+tax
+
+
+# In[15]:
+
+
+"hello"
+
+'hello'
+# In[17]:
+
+
+print('hieieieieiieiei')
+
+
+# In[18]:
+
+
+mystr="hw"
+
+
+# In[19]:
+
+
+mystr
+
+
+# In[20]:
+
+
+mystr[0]
+
+
+# In[21]:
+
+
+mystr[-1]
+
+
+# In[22]:
+
+
+mystr[1]
+
+
+# In[25]:
+
+
+mystr='abcdefghijk'
+
+
+# In[26]:
+
+
+mystr
+
+
+# In[27]:
+
+
+mystr[2:]
+
+
+# In[28]:
+
+
+mystr[:3]
+
+
+# In[29]:
+
+
+mystr[3:-5]
+
+
+# In[30]:
+
+
+mystr[3:6]
+
+
+# In[31]:
+
+
+mystr[1:3]
+
+
+# In[38]:
+
+
+mystr[::-2]
+
+
+# In[39]:
+
+
+mystr[::-1]
+
+
+# In[42]:
+
+
+if (mystr==(mystr[::-1])
+   print('yes')
+elif 
+   print('no')
+
+
+# In[ ]:
+
+
+
+
